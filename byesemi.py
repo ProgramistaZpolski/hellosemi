37a2ca54acaf925a68b221ff7c11388087c8dd19
badosz0 <30699779+badosz0@users.noreply.github.com>
2019-06-19 10:48:32 +0200

    Add files via upload

---
+++ byesemi.py
@@ -0,0 +1,25 @@
+import sys
+import os
+
+dir = sys.argv[1]
+total = 0
+print(f"Scanning {dir}")
+
+for subdir, dirs, files in os.walk(dir):
+    for file in files:
+        if file.endswith(".js"):
+            path = os.path.join(subdir, file)
+            with open(path, "r+", encoding="utf8") as file:
+                text = file.read()
+                count = text.count(";\n")
+                text = text.replace(";\n", "")
+                open(path, "w").close()
+                file.seek(0)
+                file.write(text)
+                file.close()
+            print(f"{count};\t{path}")
+            total += count
+
+print("=" * 80)
+print(f"Total ; in {dir}: {total}")
+print("=" * 80)
